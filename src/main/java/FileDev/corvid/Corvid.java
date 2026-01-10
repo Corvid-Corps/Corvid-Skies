5ae5055386d0524bc9608ccfd62140b8d848d3f1
File-Modder <mikalcorn@gmail.com>
2026-01-10 13:52:36 -0600

    added geckolib

--- src/main/java/FileDev/corvid/Corvid.java
+++ src/main/java/FileDev/corvid/Corvid.java
@@ -4,8 +4,6 @@ import FileDev.corvid.called.FlightController;
 import FileDev.corvid.called.ModEvents;
 import net.fabricmc.api.ModInitializer;
 
-
-
 public class Corvid implements ModInitializer {
     @Override
     public void onInitialize() {
